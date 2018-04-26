19dc5ef9902c8a71d40c6dc6f3143f402d84e3e5
TriMill <trimil016@gmail.com>
2018-04-26 15:29:32 -0400

    Added option to use Google Fonts or system fonts, added more font choices to menu
    Also added font-testing library

--- mini/bigtext/main.js
+++ mini/bigtext/main.js
@@ -1,10 +1,25 @@
 $(function() {
-  let text = param('text'), fg = param('fg'), bg = param('bg'), font = param('font');
-  console.log(text, fg, bg, font);
+  let text = param('text'), fg = param('fg'), bg = param('bg')
+  let font = param('font'), gft = param('gft');
   if(text && fg && bg && font) {
-    display(text, '#'+fg, '#'+bg, font);
+    display(text, '#'+fg, '#'+bg, font, gft);
     return;
   }
+  addFontsToDropdown();
+  setupColorPickers();
+});
+
+function addFontsToDropdown() {
+  let fonts = ['Arial', 'Helvetica', 'Verdana', 'Segoe UI', 'Trebuchet MS', 'Impact',
+    'Times New Roman', 'Times', 'Georgia', 'Courier', 'Consolas', 'Fixedsys Excelsior 3.01', 'FakeFont'];
+  for(let i = 0; i < fonts.length; i++) {
+    if(isFontAvailable(fonts[i])) {
+      $('#font').append($('<option value="'+fonts[i]+'">'+fonts[i]+'</option>'));
+    }
+  }
+}
+
+function setupColorPickers() {
   let foreground = $('#foreground');
   let foregroundWrapper = $('#wrapper_foreground');
   foreground.change(function() { foregroundWrapper.css('background-color', foreground.val());})
@@ -14,29 +29,39 @@ $(function() {
   let backgroundWrapper = $('#wrapper_background');
   background.change(function() { backgroundWrapper.css('background-color', background.val());})
   backgroundWrapper.css('background-color', background.val());
-});
+}
+
+
 
 function show() {
   let text = $('#text').val();
   let fg = $('#foreground').val().substring(1);
   let bg = $('#background').val().substring(1);
+  let gft = $('[name="ftype"]:checked').val() == 'ggl';
   let font = $('#font').val();
+  if(gft)
+    font = $('#gfont').val();
   window.location.href = window.location.href.split('?')[0] +
-    '?text='+(text==''?'%00':text)+'&fg='+fg+'&bg='+bg+'&font='+font;
+    '?text='+(text==''?'%00':text)+'&fg='+fg+'&bg='+bg+'&font='+font+'&gft='+gft;
 }
 
-function display(text, fg, bg, font) {
-  let body = $('#body');
+function display(text, fg, bg, font, gft) {
+  console.log(font);
+  let body = $('body');
   body.html('');
   let div = $('<div></div>');
   div.html(text);
   body.append(div);
   $('#main').remove();
   $('#style').remove();
-  body.css('font-family', font);
+  if(gft == 'true') {
+    console.log(gft);
+    $('head').append('<link href="https://fonts.googleapis.com/css?family='+ font + '" rel="stylesheet">');
+  }
+  body.css('font-family', '"'+font+'"');
   body.css('color', fg);
   body.css('background-color', bg);
-  body.css('line-height', 0);
+  body.css('line-height', 1);
   $('head').append('<link rel="stylesheet" type="text/css" href="show.css">');
 }
 
